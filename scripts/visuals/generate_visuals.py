20106040d3094a891d124b31ca19a99b75c3f48a
Olivier St-Georges <olivierstgeorges1@gmail.com>
2025-10-16 13:46:13 -0400

    fix manual bins

--- scripts/visuals/generate_visuals.py
+++ scripts/visuals/generate_visuals.py
@@ -293,7 +293,7 @@ def main():
 
     analyser_relation(hexagones, "nb_ec_supp", "nb_trajets", f"{season}/nb_ec_supp_vs_trajets",
                       correlation_csv=f"../../output/{season}/correlations.csv", decimals=0,
-                      manual_bins=[0, 1, 2, 3,4, 5, 6])
+                      manual_bins=[0, 1, 2, 3, 6])
 
     analyser_zonage(hexagones, output_path=f"../../output/{season}/zonage.png")
 
